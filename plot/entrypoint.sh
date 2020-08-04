4577742fc6579ca284688cb7fbacd538b2b42403
Masashi SHIBATA <c-bata@users.noreply.github.com>
2020-08-05 03:15:46 +0900

    More plot options (#3)

--- plot/entrypoint.sh
+++ plot/entrypoint.sh
@@ -2,12 +2,44 @@
 
 set -e
 
+args="-o ./output/"
+
 if [ $2 != "false" ]; then
-  cat $1 | ./kurobako plot curve --errorbar -o ./output/
-else
-  cat $1 | ./kurobako plot curve -o ./output/
+  args="$args --errorbar"
 fi
+
+if [ $3 != "false" ]; then
+  args="$args --ylogscale"
+fi
+
+if [ $4 != "none" ]; then
+  args="$args --xmin $4"
+fi
+
+if [ $5 != "none" ]; then
+  args="$args --xmax $5"
+fi
+
+if [ $6 != "none" ]; then
+  args="$args --ymin $6"
+fi
+
+if [ $7 != "none" ]; then
+  args="$args --ymax $7"
+fi
+
+if [ $8 != "none" ]; then
+  args="$args --width $8"
+fi
+
+if [ $9 != "none" ]; then
+  args="$args --height $9"
+fi
+
+
+cat $1 | ./kurobako plot curve $args
 
 imagepath=$(ls ./output/*.png | head -1)
 
 echo ::set-output name=image-path::$imagepath
+
